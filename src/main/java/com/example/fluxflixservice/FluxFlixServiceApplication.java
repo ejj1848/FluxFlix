0bfbdd72f61fa2475d728d65d64f542f9db8eb6a
Eric.Johnson <eric.johnson@astontech.com>
2018-01-16 11:44:21 -0600

    Coolest Comment Ever was Added

--- src/main/java/com/example/fluxflixservice/FluxFlixServiceApplication.java
+++ src/main/java/com/example/fluxflixservice/FluxFlixServiceApplication.java
@@ -3,6 +3,7 @@ package com.example.fluxflixservice;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+//This is the coolest demo ever!!!
 @SpringBootApplication
 public class FluxFlixServiceApplication {
 
