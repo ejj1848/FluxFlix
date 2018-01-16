43b2f5c8323be0e56af9cd5f312e29b29e885e55
bairdh <heather.baird@astontech.com>
2018-01-16 12:20:27 -0600

    try number 2

--- src/main/java/com/example/fluxflixservice/FluxFlixServiceApplication.java
+++ src/main/java/com/example/fluxflixservice/FluxFlixServiceApplication.java
@@ -3,7 +3,7 @@ package com.example.fluxflixservice;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-//Alex
+//Heather
 //David Pena
 @SpringBootApplication
 public class FluxFlixServiceApplication {
